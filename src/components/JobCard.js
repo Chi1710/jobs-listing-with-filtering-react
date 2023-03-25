df328bea18066b35b1a8107ac7f82db6eaa0c477
Chi <chiyantsestudio@gmail.com>
2023-03-25 16:08:58 -0400

    add images file for the business logos and data in public. fetch data with axios, display each job in JobCard

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./JobCard.css"
 
-const JobCard = () => {
+const JobCard = ( {job} ) => {
+
   return (
-    <div>JobCard</div>
+    <div>
+    <div className='logo'>
+    <img src={job.logo} alt={job.company} />
+    </div>
+
+    <div>
+    <h3>{job.company}</h3>
+    <h3>{job.new ? "NEW!" : ""}</h3>
+    <h3>{job.featured ? "FEATURED" : ""}</h3> 
+    <h1>{job.position}</h1>
+    <h3>{job.postedAt}</h3>
+    <h3>{job.contract}</h3>
+    <h3>{job.location}</h3>
+    </div>
+
+
+    </div>
   )
 }
 
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Jobs.css"
+import JobCard from './JobCard'
+import axios from "axios";
+
+
 
 const Jobs = () => {
+  const[isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect( () => {
+    axios
+    .get("data.json")
+    .then(result => {
+      setJobs(result.data)
+      setIsLoading(false)
+    }).catch(error => {
+      setError(error.message)
+      setIsLoading(false)
+    })
+  }, [])
+
+  if(isLoading){
+    return <h2> Loading... </h2>
+  }
+  if(error){
+    return <h2>{error}</h2>
+  }
+
   return (
-    <div>Jobs</div>
+    <>
+    <h1>Jobs</h1>
+    {jobs.map( (job) => (
+      <JobCard key={job.id}
+                job={job} 
+      />
+    ))}
+
+    </>
   )
 }
 
