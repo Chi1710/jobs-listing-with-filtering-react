2653c047bc3847cae25813d8347da6680ab48eb1
Andrea <andrearivas4cinfor@gmail.com>
2023-03-29 11:24:19 +0200

    style job card part 1

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -4,22 +4,26 @@ import "./JobCard.css"
 const JobCard = ( {job} ) => {
 
   return (
-    <div>
-    <div className='logo'>
-    <img src={job.logo} alt={job.company} />
-    </div>
+    <div className="job-card">
+      <div className="logo">
+        <img src={job.logo} alt={job.company} />
+      </div>
 
-    <div>
-    <h3>{job.company}</h3>
-    <h3>{job.new ? "NEW!" : ""}</h3>
-    <h3>{job.featured ? "FEATURED" : ""}</h3> 
-    <h1>{job.position}</h1>
-    <h3>{job.postedAt}</h3>
-    <h3>{job.contract}</h3>
-    <h3>{job.location}</h3>
-    </div>
-
-
+      <div className="job-details">
+        <div className="job-company">
+          <h3 className='company-name'>{job.company}</h3>
+          <h3 className='new label'>{job.new ? 'NEW!' : ''}</h3>
+          <h3 className='featured label'>{job.featured ? 'FEATURED' : ''}</h3>
+        </div>
+        <h1 className='job-position'>{job.position}</h1>
+        <div className="job-conditions">
+          <h3>{job.postedAt}</h3>
+          <h3>{job.contract}</h3>
+          <h3>{job.location}</h3>
+        </div>
+        <div className='line'></div>
+        <div className='filter'>filters here</div>
+      </div>
     </div>
   )
 }
