d0ae45d895ea41cfe8097e20c20eaa725d1c90c3
Andrea <andrearivas4cinfor@gmail.com>
2023-03-29 15:36:54 +0200

    Fix aligment

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -6,38 +6,40 @@ const JobCard = ( {job} ) => {
   return (
     <div className="job-card">
       <img className="logo" src={job.logo} alt={job.company} />
-      <div className="job-details">
-        <div className="job-company">
-          <h3 className="company-name">{job.company}</h3>
-          {job.new && <h3 className="new label">NEW!</h3>}
-          {job.featured && <h3 className="featured label">FEATURED</h3>}
+      <div className="container-div">
+        <div className="job-details">
+          <div className="job-company">
+            <h3 className="company-name">{job.company}</h3>
+            {job.new && <h3 className="new label">NEW!</h3>}
+            {job.featured && <h3 className="featured label">FEATURED</h3>}
+          </div>
+          <h1 className="job-position">{job.position}</h1>
+          <ul className="job-conditions">
+            <li>{job.postedAt}</li>
+            <li>{job.contract}</li>
+            <li>{job.location}</li>
+          </ul>
+          <div className="horizontal-line"></div>
         </div>
-        <h1 className="job-position">{job.position}</h1>
-        <ul className="job-conditions">
-          <li>{job.postedAt}</li>
-          <li>{job.contract}</li>
-          <li>{job.location}</li>
-        </ul>
-        <div className="horizontal-line"></div>
-      </div>
-      <div className="filter-tags">
-        <button className="filter-tag" type="button">
-          {job.role}
-        </button>
-        <button className="filter-tag" type="button">
-          {job.level}
-        </button>
-        {job.languages.map((language, index) => (
+        <div className="filter-tags">
           <button className="filter-tag" type="button">
-            {language}
+            {job.role}
           </button>
-        ))}
-        {job.tools &&
-          job.tools.map((tool, index) => (
+          <button className="filter-tag" type="button">
+            {job.level}
+          </button>
+          {job.languages.map((language, index) => (
             <button className="filter-tag" type="button">
-              {tool}
+              {language}
             </button>
           ))}
+          {job.tools &&
+            job.tools.map((tool, index) => (
+              <button className="filter-tag" type="button">
+                {tool}
+              </button>
+            ))}
+        </div>
       </div>
     </div>
   )
