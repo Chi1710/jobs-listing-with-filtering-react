7172d7a2c778a56911949a8a3072a773f5c3143d
Andrea <andrearivas4cinfor@gmail.com>
2023-03-29 12:54:54 +0200

    style job cards desktop version

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -5,27 +5,22 @@ const JobCard = ( {job} ) => {
 
   return (
     <div className="job-card">
-        <img className="logo" src={job.logo} alt={job.company} />
-     
-
+      <img className="logo" src={job.logo} alt={job.company} />
       <div className="job-details">
         <div className="job-company">
-          <h3 className='company-name'>{job.company}</h3>
-          {job.new && (
-             <h3 className='new label'>NEW!</h3>   )}
-             {job.featured && (
-              <h3 className='featured label'>FEATURED</h3>
-             )}
+          <h3 className="company-name">{job.company}</h3>
+          {job.new && <h3 className="new label">NEW!</h3>}
+          {job.featured && <h3 className="featured label">FEATURED</h3>}
         </div>
-        <h1 className='job-position'>{job.position}</h1>
+        <h1 className="job-position">{job.position}</h1>
         <ul className="job-conditions">
           <li>{job.postedAt}</li>
           <li>{job.contract}</li>
           <li>{job.location}</li>
         </ul>
-        <div className='horizontal-line'></div>
-        <div className='filter'>filters here</div>
+        <div className="horizontal-line"></div>
       </div>
+      <div className="filter">filters here</div>
     </div>
   )
 }
