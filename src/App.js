70155c18274054557fd0ea2185b972bea628da52
Andrea <andrearivas4cinfor@gmail.com>
2023-04-04 17:19:02 +0200

    fix code to be able to add same filter just once

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Jobs from './components/Jobs';
-
-import Footer from './components/Footer';
 import { useState } from 'react'
 import { FilterContext } from './components/createContext'
 
 function App() {
   const [filters, setFilters] = useState([])
 
-  const addFilter = (filter) => {
-    setFilters([...filters, filter])
-  }
-
-  const removeFilter = (filter) => {
-    setFilters(filters.filter((f) => f !== filter))
-  }
-
+ const addFilter = (filter) => {
+   if (!filters.includes(filter)) {
+     setFilters([...filters, filter])
+   }
+ }
   return (
-    <FilterContext.Provider value={{ filters, addFilter, removeFilter }}>
+    <FilterContext.Provider value={{ filters, addFilter}}>
      <Jobs />
     </FilterContext.Provider>
   )
