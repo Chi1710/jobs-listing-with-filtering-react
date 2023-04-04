fdc5abc4bc03dcb8d4435e2b92f49e91eb933b06
Andrea <andrearivas4cinfor@gmail.com>
2023-04-04 17:07:33 +0200

    Fix code

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -9,7 +9,7 @@ const Jobs = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [jobs, setJobs] = useState([])
   const [error, setError] = useState('')
-
+//-- Take filters array from the Context created.
   const { filters } = useContext(FilterContext)
 
   useEffect(() => {
@@ -25,17 +25,14 @@ const Jobs = () => {
       })
   }, [])
 
-  const handleFilter = (tag) => {
-  }
-
-
+//-- Create new Array that filters the jobs. For each job, creates an array (jobTags) with the tags of that job, and then returns all the elements in the filters array that has the filter
 const filteredJobs = jobs.filter((job) => {
   const jobTags = [
     job.role,
     job.level,
     ...job.languages,
     ...(job.tools || []),
-  ];
+  ]
   return filters.length === 0 || filters.every((filter) => jobTags.includes(filter));
 });
 
@@ -43,7 +40,7 @@ const filteredJobs = jobs.filter((job) => {
   return (
     <>
       <header className="header">
-        <FilterTags filters={filters} handleFilter={handleFilter} />
+        <FilterTags />
       </header>
       <div className="jobs-container">
         {filteredJobs.length === 0 ? (
--- src/components/createContext.js
+++ src/components/createContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react'
-
+//-- Create context for the filtering (here we can add remove and clear filters)
 export const FilterContext = createContext({
   filters: [],
   addFilter: () => {},
